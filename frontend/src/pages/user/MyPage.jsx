4d030352165b5135d33eed0bbb9d16a933464547
jivvoor <wldn040839@gmail.com>
2026-03-02 15:18:07 +0900

    fix: 사용하지 않는 Link import 제거

--- frontend/src/pages/user/MyPage.jsx
+++ frontend/src/pages/user/MyPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { getMe, getDashboard } from '../../api/user'
 import MyReservations from './MyReservations'
 
