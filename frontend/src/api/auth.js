577a5263955423a13cde27c7b2036c36c184bd53
JunHyun <90903322+junhyun1001@users.noreply.github.com>
2026-03-06 17:38:16 +0900

    [refactor] 프론트엔드 JWT 쿠키 전송으로 변경 (#103)
    * refactor: jwt 쿠키 전송으로 변경
    
    #102
    
    * refactor: jwt 쿠키 전송으로 변경
    
    #102
    
    * refactor: jwt 쿠키 전송으로 변경
    
    #102
    
    * refactor: 회원가입시 401 에러 로그인 페이지로 리다이렉트 되는 문제 수정
    
    #102
    
    * chore: submodule update
    
    #102
    
    * refactor: refresh 실패 시 대기 중인 요청들이 영원히 pending 되는 문제 수정
    
    #102
    
    * refactor: biome lint 오류 수정
    
    #102
    
    * refactor: userId 제거
    
    #102

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,6 @@
 import { authApi } from './client'
 
 export const authService = {
-    signup: (signupData) => authApi.post('/auth/signup', signupData),
-    login: (loginData) => authApi.post('/auth/login', loginData),
-    reissue: (refreshToken) => authApi.post('/auth/reissue', { refreshToken }),
+    signup: (signupData) => authApi.post('/auth/signup', signupData, { skipAuthRedirect: true }),
+    login: (loginData) => authApi.post('/auth/login', loginData, { skipAuthRedirect: true }),
 }
