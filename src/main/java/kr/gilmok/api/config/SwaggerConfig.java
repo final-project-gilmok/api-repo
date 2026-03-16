2091d1fab89b50a088a1589cf76e922fc5ff5b10
jivvoor <wldn040839@gmail.com>
2026-03-16 11:14:30 +0900

    feat: Config 설정 추가

---
+++ src/main/java/kr/gilmok/api/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package kr.gilmok.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gilmok API")
+                        .description("길목 관리자/사용자 API 문서")
+                        .version("v1")
+                        .contact(new Contact().name("Gilmok Team")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("admin")
+                .pathsToMatch("/admin/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/events/**")
+                .build();
+    }
+}
+
